9505ad2a4e5c88bd71e232d434ebe733d0c2e1e7
zhouhang <2006zhouhang@163.com>
2016-04-13 16:03:54 +0800

    fix for wechart share again

--- res/Btrade-mobile/static/js/wechart.js
+++ res/Btrade-mobile/static/js/wechart.js
@@ -54,25 +54,4 @@ wx.error(function(res){
     console.log(res);
 });
 
-function readyWeixin(data){
-    wx.ready(function () {
-        setupWeixinShare({
-            title: data.title, // 分享标题
-            desc: data.desc, // 分享描述
-            link: data.link, // 分享链接
-            imgUrl: data.imgUrl, // 分享图标
-            type: data.type, // 分享类型,music、video或link，不填默认为link
-            dataUrl: data.Url, // 如果type是music或video，则要提供数据链接，默认为空
-            success: function () {
-                // 用户确认分享后执行的回调函数
-                console.log(data);
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
-                console.log("cancel");
-            }
-        });
-    });
-}
-
 bootstrap();
