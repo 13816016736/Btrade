980186eb79f5e89148fe1baea6a2710b96c5f67c
unknown <2011zhouhang@gmail.com>
2016-05-20 21:47:17 +0800

    supplier update

--- res/Btrade-scrapy/tradespider/tradespider/yt1998.py
+++ res/Btrade-scrapy/tradespider/tradespider/yt1998.py
@@ -17,14 +17,14 @@ import MySQLdb
 with open("G:\\Btrade\\res\\Btrade-scrapy\\tradespider\\tradespider\\ytSupplyList.json", "r") as f:
     d1 = json.load(f)
     for data in d1["data"]:
-        if data["ycnam"] == u'毛知母':
+        if data["ycnam"] == u'桂皮':
             print data["ycnam"]
             conn = MySQLdb.connect(host='localhost',user='root',passwd='ycg20160401',db='yaocai',port=3306, charset="utf8")
             cursor = conn.cursor()
 
             # cursor.execute("select id from variety where name = %s", (data["ycnam"],))
             # variety = cursor.fetchone()
-            variety = [453]
+            variety = [605]
             if variety and len(variety) == 1:
                 varietyid = variety[0]
                 cursor.execute("select id,variety from supplier where mobile = %s", (data["mob"],))
