9c5dd50d05f88dbdf7f048dd1a9d0a09870d6add
frank <frank1989678@126.com>
2016-07-07 15:13:10 +0800

    1、首页二维码换成js生成 2、图片预览改用lightbox

--- res/Btrade-pc/static/js/common.js
+++ res/Btrade-pc/static/js/common.js
@@ -1,15 +1,25 @@
 $(function() {
 
-	var temp = '<div class="modal fade"id="jModalImg" tabindex="-1" role="dialog" aria-labelledby="tit1"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="tit1">图片</h4></div><div class="modal-body"><div class="preview"><div class="bd"><ul class="yc-ul"></ul></div><div class="ctrl prev"></div><div class="ctrl next"></div></div></div></div></div></div>';
+	// 初始化图片预览容器
+	function initModel() {
+		if ($('#jModalImg').length === 0) {
+			var modal = '<div class="modal fade"id="jModalImg" tabindex="-1" role="dialog" aria-labelledby="tit1"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="tit1">图片</h4></div><div class="modal-body"><div class="preview"><div class="bd"><ul class="yc-ul"></ul></div><div class="ctrl prev"></div><div class="ctrl next"></div></div></div></div></div></div>';
+			$('body').append(modal);
+		} 		
+	}
 
-	// 图片放大
-	function showBigPic(imgUrls, index) {
-	    var _result = '<div class="preview"><div class="bd"><ul class="yc-ul">';
+	// 图片预览
+	function imagePreview(imgUrls, index) {
+	    initModel();
+
+	    var modal = ['<div class="preview"><div class="bd"><ul class="yc-ul">'];
 	    $.each(imgUrls, function(i, src) {
-	        _result += '<li><img src="' + src + '"></li>';
+	        modal.push('<li><img src="', src, '"></li>');
 	    });
-	    _result += '</ul></div><div class="ctrl prev"></div><div class="ctrl next"></div>'
-	    $('#jModalImg .modal-body').html(_result);
+	    modal.push('</ul></div><div class="ctrl prev"></div><div class="ctrl next"></div>');
+
+	    $('#jModalImg .modal-body').html(modal.join(''));
+	    $('#jModalImg').modal();
 
 	    $('.preview').slide({
 	        mainCell: '.bd ul',
@@ -20,21 +30,14 @@ $(function() {
 	        trigger: 'click'
 	    }); 
 	}
-	function initModel() {
-		if ($('#jModalImg').length === 0) {
-			$('body').append(temp);
-		}
-	}
+
 	$('body').on('click', '.thumb img', function() {
-	    var index = $(this).index();
-	    var imgUrls = [];
+	    var index = $(this).index(),
+	    	imgUrls = [];
 	    $(this).parent().find('img').each(function() {
 	        imgUrls.push($(this).data('src'));
-	    });
-		
-		initModel();
-	    $('#jModalImg').modal();
-	    showBigPic(imgUrls, index);
+	    });			
+	    imagePreview(imgUrls, index);
 	    return false;
 	});
 
