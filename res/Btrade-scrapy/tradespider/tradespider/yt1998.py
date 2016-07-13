0906da24a87b1d9703137403a8d0d3701df315b6
chaochaoxiao <376161552@qq.com>
2016-07-13 16:54:29 +0800

    更新supplier

--- res/Btrade-scrapy/tradespider/tradespider/yt1998.py
+++ res/Btrade-scrapy/tradespider/tradespider/yt1998.py
@@ -14,17 +14,17 @@ import MySQLdb
 #     cursor.execute("update supplier set variety = %s where id = %s ", (var, s[0]))
 # conn.commit()
 
-with open("E:\\work\\Btrade\\res\\Btrade-scrapy\\tradespider\\tradespider\\ytSupplyList.json", "r") as f:
+with open("F:\\Btrade\\res\\Btrade-scrapy\\tradespider\\tradespider\\ytSupplyList.json", "r") as f:
     d1 = json.load(f)
     for data in d1["data"]:
-        if data["ycnam"] == u'辽细辛':
+        if data["ycnam"] == u'夜交藤':
             print data["ycnam"]
             conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='ycg20160401',db='yaocai',port=3306, charset="utf8")
             cursor = conn.cursor()
 
             # cursor.execute("select id from variety where name = %s", (data["ycnam"],))
             # variety = cursor.fetchone()
-            variety = [519]
+            variety = [629]
             if variety and len(variety) == 1:
                 varietyid = variety[0]
                 cursor.execute("select id,variety from supplier where mobile = %s", (data["mob"],))
