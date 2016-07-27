580093a533b977b7be195dee8932940e97b9cf3b
chaochaoxiao <376161552@qq.com>
2016-07-27 10:47:13 +0800

    修改脚本

--- res/Btrade-public/celery.sh
+++ res/Btrade-public/celery.sh
@@ -1,3 +1,3 @@
 sudo celery -A pushengine worker -f pushengine/log/celery.log -l info
 
-celery -A pushengine beat -f pushengine/log/celery.log -s pushengine/celerybeat-schedule
+celery -A pushengine beat -f pushengine/log/beat.log -s pushengine/celerybeat-schedule
