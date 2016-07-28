852bbbcb87770be368c89fedabe27243a7c5a200
chaochaoxiao <376161552@qq.com>
2016-07-28 14:50:30 +0800

    测试占比

--- res/Btrade-public/pushengine/test.py
+++ res/Btrade-public/pushengine/test.py
@@ -1,7 +1,7 @@
 #coding:utf8
-from pushengine.tasks import analysis_notify,analysis_record,sendkafka
-from datetime import timedelta,datetime
-import time
+#from pushengine.tasks import analysis_notify,analysis_record,sendkafka
+#from datetime import timedelta,datetime
+#import time
 #r = analysis_record.apply_async()
 
 #print "Result:",r.get()
@@ -22,5 +22,34 @@ import time
 #pid="371"
 #purchaseid="146112075263"
 #reply_wx_notify(openid,num, name, price, unit,pid,purchaseid)
+
+from database import database
+sqldb = database.instance().get_session()
+num=6
+all_users=sqldb.query("select * from users")
+all_num=len(all_users)
+gt_num=0
+for item in all_users:
+    if item["varietyids"]!="":
+        count=item["varietyids"].split(",")
+        if count>num:
+            gt_num+=1
+print gt_num
+print all_num
+print gt_num/all_num
+
+
+all_users=sqldb.query("select * from supplier")
+all_num=len(all_users)
+gt_num=0
+for item in all_users:
+    if item["variety"]!="":
+        count=item["variety"].split(",")
+        if count>num:
+            gt_num+=1
+print gt_num
+print all_num
+print gt_num/all_num
+
 
 
