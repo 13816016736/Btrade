e1106ecebc86a1caaa7e83a45448603a1668053a
chaochaoxiao <376161552@qq.com>
2016-08-04 11:11:34 +0800

    sh更改

--- res/Btrade-public/celery.sh
+++ res/Btrade-public/celery.sh
@@ -1,3 +1,3 @@
-sudo celery -A pushengine worker -f pushengine/log/celery.log -l info
+sudo nohup celery -A pushengine worker -f pushengine/log/celery.log -l info &
 
-sudo celery -A pushengine beat -f pushengine/log/beat.log -s pushengine/celerybeat-schedule
+sudo nohup celery -A pushengine beat -f pushengine/log/beat.log -s pushengine/celerybeat-schedule &
