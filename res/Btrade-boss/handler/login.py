0f4a33267b4f9f76d34e040e230411be795cfeef
chaochaoxiao <376161552@qq.com>
2016-08-08 19:01:43 +0800

    user 提交有问题

--- res/Btrade-boss/handler/login.py
+++ res/Btrade-boss/handler/login.py
@@ -18,17 +18,21 @@ class LoginHandler(BaseHandler):
             self.render("login.html", error="用户名或邮箱不能为空", next_url=self.get_argument("next", "/"))
             return
         author = self.db.get("SELECT * FROM admin WHERE username = %s", username)
-        if not author:
+        if not author :
             self.render("login.html", error="用户名不存在", next_url=self.get_argument("next", "/")
                         , username=username)
             return
-        if md5(str(password+config.salt)) == author.password:
-            self.session["adminid"] = author.id
-            self.session["admin"] = self.get_argument("username")
-            self.session.save()
-            self.redirect(self.get_argument("next_url", "/"))
         else:
-            self.render("login.html", error="用户名或密码错误", next_url=self.get_argument("next", "/"))
+            if author.status==1:
+                if md5(str(password+config.salt)) == author.password:
+                    self.session["adminid"] = author.id
+                    self.session["admin"] = self.get_argument("username")
+                    self.session.save()
+                    self.redirect(self.get_argument("next_url", "/"))
+                else:
+                    self.render("login.html", error="用户名或密码错误", next_url=self.get_argument("next", "/"))
+            else:
+                self.render("login.html", error="该用户被禁用", next_url=self.get_argument("next", "/"))
 
 class LogoutHandler(BaseHandler):
     def get(self):
